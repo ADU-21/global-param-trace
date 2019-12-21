17e9d9669a5f76f08b380fc8c0f348a55782373d
ADU <LukeDu571@gmail.com>
2019-12-21 12:37:39 +0800

    Init project

--- src/main/java/com/adu21/spring/boot/global/param/trace/util/MessageUtil.java
+++ src/main/java/com/adu21/spring/boot/global/param/trace/util/MessageUtil.java
@@ -5,6 +5,7 @@ import com.google.common.collect.ImmutableMap;
 /**
  * @author LukeDu
  * @date 2019/12/21
+ * Support different language with single error code
  */
 public class MessageUtil {
     private static final ImmutableMap<Long, String> errorMap = ImmutableMap.of(
